2414b1b1c83ecc5363181d2fb5e67dca0f3cab07
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2022-11-09 11:22:53 +0200

    Updated gallery.js
    1) Added modern way to render components to DOM (using .map() and template string;
    2) Added list-type-none for ul.gallery due to using tag "li", not tag "div"

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,31 +1,33 @@
+import 'simplelightbox/dist/simple-lightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-import 'simplelightbox/dist/simple-lightbox';
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-import 'simplelightbox/dist/simple-lightbox.min.css';
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li>
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+    })
+    .join('');
+}
 
-const gallery = document.querySelector('.gallery');
-const items = [];
-
-galleryItems.forEach(element => {
-  const galleryLink = document.createElement('a');
-  galleryLink.className = 'gallery__link';
-  galleryLink.href = element.original;
-  const galleryImage = document.createElement('img');
-  galleryImage.className = 'gallery__image';
-  galleryImage.src = element.preview;
-  galleryImage.setAttribute('title', element.description);
-  galleryImage.alt = element.description;
-
-  galleryLink.append(galleryImage);
-  items.push(galleryLink);
-});
-gallery.append(...items);
-
-new SimpleLightbox('.gallery a', {
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
   captionDelay: 250,
+  captionPosition: 'bottom',
 });
